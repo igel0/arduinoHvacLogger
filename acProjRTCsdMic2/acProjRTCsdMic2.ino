2198a6ebcc17545c65cd4c65bad5125685145db7
igel0 <igeliebter@gmail.com>
2016-07-12 18:16:56 -0400

    original working file

---
+++ acProjRTCsdMic2/acProjRTCsdMic2.ino
@@ -0,0 +1,178 @@
+// Date and time functions using a DS1307 RTC connected via I2C and Wire lib
+#include <Wire.h>
+#include "RTClib.h"
+#include <SD.h>
+
+#if defined(ARDUINO_ARCH_SAMD)
+// for Zero, output on USB Serial console, remove line below if using programming port to program the Zero!
+   #define Serial SerialUSB
+#endif
+
+//function declarations
+bool mic_sample();
+
+//for rtc
+RTC_DS1307 rtc;
+
+//for SD card
+File myFile;
+
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
+void setup () {
+
+//general init
+
+#ifndef ESP8266
+  while (!Serial); // for Leonardo/Micro/Zero
+#endif
+
+  Serial.begin(57600);
+
+//for RTC init
+  
+  if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    while (1);
+  }
+
+  if (! rtc.isrunning()) {
+    Serial.println("RTC is NOT running!");
+    // following line sets the RTC to the date & time this sketch was compiled
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    // This line sets the RTC with an explicit date & time, for example to set
+    // January 21, 2014 at 3am you would call:
+    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+  }
+
+//  for SD card init
+
+  Serial.print("Initializing SD card...");
+  // On the Ethernet Shield, CS is pin 4. It's set as an output by default.
+  // Note that even if it's not used as the CS pin, the hardware SS pin 
+  // (10 on most Arduino boards, 53 on the Mega) must be left as an output 
+  // or the SD library functions will not work. 
+   pinMode(10, OUTPUT);
+ 
+  if (!SD.begin(10)) {
+    Serial.println("initialization failed!");
+    return;
+  }
+  Serial.println("initialization done.");
+ 
+  // open the file. note that only one file can be open at a time,
+  // so you have to close this one before opening another.
+  myFile = SD.open("test2.txt", FILE_WRITE);
+
+ if (!myFile) { 
+    // if the file didn't open, print an error:
+    Serial.println("error opening test.txt");
+  }
+
+}
+
+void loop () {
+
+   
+ 
+    Serial.print("Writing to test.txt...");
+//    myFile.println("testing 1, 2, 3.");
+//  // close the file:
+//    myFile.close();
+    Serial.println("done.");
+
+    DateTime now = rtc.now(); //get time
+    bool micReading = mic_sample(); //read mic
+
+//    first print to screen
+    Serial.print(now.year(), DEC);
+    Serial.print('/');
+    Serial.print(now.month(), DEC);
+    Serial.print('/');
+    Serial.print(now.day(), DEC);
+    Serial.print(" ");
+  
+    Serial.print(now.hour(), DEC);
+    Serial.print(':');
+    Serial.print(now.minute(), DEC);
+    Serial.print(':');
+    Serial.print(now.second(), DEC);
+//    add unixtime
+//    Serial.print(' ');
+//    Serial.print(now.unixtime(), DEC);
+
+
+
+    Serial.print(" (");
+    Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
+    Serial.print(") ");
+    Serial.print(micReading);
+    Serial.println();
+    
+    
+    Serial.println();
+
+//    then print to sd card
+    myFile.print(now.year(), DEC);
+    myFile.print('/');
+    myFile.print(now.month(), DEC);
+    myFile.print('/');
+    myFile.print(now.day(), DEC);
+    myFile.print(" ");
+  
+    myFile.print(now.hour(), DEC);
+    myFile.print(':');
+    myFile.print(now.minute(), DEC);
+    myFile.print(':');
+    myFile.print(now.second(), DEC);
+
+    myFile.print(" (");
+    myFile.print(daysOfTheWeek[now.dayOfTheWeek()]);
+    myFile.print(") ");
+    myFile.print(micReading);
+    myFile.println();
+
+//    save it to the sd card
+    myFile.flush();
+
+    delay(10000);
+   
+}
+bool mic_sample() 
+{
+   const int sampleWindow = 50; // Sample window width in mS (50 mS = 20Hz)
+   unsigned int sample;
+   unsigned long startMillis= millis();  // Start of sample window
+   unsigned int peakToPeak = 0;   // peak-to-peak level
+
+   unsigned int signalMax = 0;
+   unsigned int signalMin = 1024;
+   bool onOff;
+
+   // collect data for 50 mS
+   while (millis() - startMillis < sampleWindow)
+   {
+      sample = analogRead(0);
+      if (sample < 1024)  // toss out spurious readings
+      {
+         if (sample > signalMax)
+         {
+            signalMax = sample;  // save just the max levels
+         }
+         else if (sample < signalMin)
+         {
+            signalMin = sample;  // save just the min levels
+         }
+      }
+   }
+   peakToPeak = signalMax - signalMin;  // max - min = peak-peak amplitude
+   double volts = (peakToPeak * 3.3) / 1024;  // convert to volts
+
+   Serial.println(volts);
+
+   if (volts >= .04) onOff = 1;
+   else onOff = 0;
+   
+
+   return onOff;
+}
